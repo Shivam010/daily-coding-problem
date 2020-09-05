dbba86d273413d6b4579d5c0c297f211fa2e4b90
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-05 22:44:04 +0530

    Solve 003

--- 001/code.cpp
+++ 001/code.cpp
@@ -1,6 +1,6 @@
 /*
 Daily Coding Challenge #001
---------------------------
+---------------------------
 Given a list of numbers and a number k, return whether any two numbers from the
 list add up to k.
 
--- 002/code.cpp
+++ 002/code.cpp
@@ -1,6 +1,6 @@
 /*
 Daily Coding Challenge #002
---------------------------
+---------------------------
 Given an array of integers, return a new array such that each element at index i
 of the new array is the product of all the numbers in the original array except
 the one at i.
