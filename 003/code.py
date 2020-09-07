42ec55d28519ac03e5ed49de66c58bc5c591651e
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-07 22:25:33 +0530

    Solve 005 & format question 003

--- 003/code.py
+++ 003/code.py
@@ -3,7 +3,9 @@
 """
 Daily Coding Challenge #003
 ---------------------------
-Given the root to a binary tree, implement serialize(root), which serializes the tree into a string, and deserialize(s), which deserializes the string back into the tree.
+Given the root to a binary tree, implement serialize(root), which serializes 
+the tree into a string, and deserialize(s), which deserializes the string 
+back into the tree.
 
 For example, given the following Node class
 
