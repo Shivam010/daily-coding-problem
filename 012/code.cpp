fcd21626ce7f36faf7e6e3efedf38e61275ba64e
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-14 23:37:50 +0530

    Solve 012

--- 012/code.cpp
+++ 012/code.cpp
@@ -1,7 +1,23 @@
 /*
 Daily Coding Challenge #012
 ---------------------------
+This problem was asked by Amazon.
 
+There exists a staircase with N steps, and you can climb up either 1 or 2 steps
+at a time. Given N, write a function that returns the number of unique ways you
+can climb the staircase. The order of the steps matters.
+
+For example, if N is 4, then there are 5 unique ways:
+
+    1, 1, 1, 1
+    2, 1, 1
+    1, 2, 1
+    1, 1, 2
+    2, 2
+
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb
+any number from a set of positive integers X? For example, if X = {1, 3, 5}, you
+could climb 1, 3, or 5 steps at a time.
 
 Run Using
 ---------
@@ -10,20 +26,47 @@ g++ 012/code.cpp -o bin/out && ./bin/out < 012/in.txt > 012/out.txt
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define mk make_pair
-#define pb push_back
-#define Pi pair<int, int>
-#define Pl pair<long long, long long>
-#define vi vector<int>
-#define vl vector<long long>
-#define vb vector<bool>
-#define vp vector<pair<int, int>>
-#define vpl vector<pair<long long, long long>>
-#define F first
-#define S second
-#define min(a, b) ((a < b) ? a : b)
-const ll mod = 1000000007;
+#define vl vector<ll>
+vl sol(1000000, -1);
+
+ll solve(ll n, vl &list) {
+    if (sol[n] != -1) return sol[n];
+    ll len = list.size();
+    if (n <= 0 || len == 0) {
+        sol[n] = 0;
+        return 0;
+    }
+    if (len == 1) {
+        sol[n] = (n / list[0]) * (n % list[0] == 0);
+        return sol[n];
+    }
+    ll ans = 0;
+
+    for (ll i = 0; i < len; i++) {
+        if (list[i] > n) continue;
+        if (n - list[i] == 0) {
+            ans += 1;
+            continue;
+        }
+        ans += solve(n - list[i], list);
+    }
+    sol[n] = ans;
+    return ans;
+}
 
 int main() {
-
+    ll t;
+    cin >> t;
+    while (t--) {
+        sol.clear();
+        sol = vl(1000000, -1);
+        ll n, noSteps;
+        cin >> n >> noSteps;
+        vl list(noSteps, 0);
+        for (ll i = 0; i < noSteps; i++) {
+            cin >> list[i];
+        }
+        sort(list.begin(), list.end());
+        cout << solve(n, list) << endl;
+    }
 }
