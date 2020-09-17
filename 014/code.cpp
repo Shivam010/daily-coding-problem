8720273566b1631f4c5db3661ac00f259ad44117
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-17 23:47:23 +0530

    Solve 014

--- 014/code.cpp
+++ 014/code.cpp
@@ -1,7 +1,12 @@
 /*
 Daily Coding Challenge #014
 ---------------------------
+This problem was asked by Google.
 
+The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a
+Monte Carlo method.
+
+Hint: The basic equation of a circle is x2 + y2 = r2.
 
 Run Using
 ---------
@@ -10,20 +15,18 @@ g++ 014/code.cpp -o bin/out && ./bin/out < 014/in.txt > 014/out.txt
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define mk make_pair
-#define pb push_back
-#define Pi pair<int, int>
-#define Pl pair<long long, long long>
-#define vi vector<int>
-#define vl vector<long long>
-#define vb vector<bool>
-#define vp vector<pair<int, int>>
-#define vpl vector<pair<long long, long long>>
-#define F first
-#define S second
-#define min(a, b) ((a < b) ? a : b)
-const ll mod = 1000000007;
+const ll LIMIT = 100000007;
 
 int main() {
-
+    double cnt = 0, x = 0, y = 0;
+    for (ll i = 0; i < LIMIT; i++) {
+        x = double(rand()) / double(RAND_MAX);
+        y = double(rand()) / double(RAND_MAX);
+        // cout << x << " " << y << endl;
+        if (x * x + y * y <= 1) cnt++;
+    }
+    // probability that x,y is in first quadrant = (π*1*1/4)/1*1 = π/4
+    // => π/4 = cnt/LIMIT
+    double pi = cnt * 4.0 / (LIMIT);
+    cout << "Value of pi " << pi << endl;
 }
