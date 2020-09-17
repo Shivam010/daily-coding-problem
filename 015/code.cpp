08f1f99c744ba5360b0ed3d531337dc30b2b0064
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-17 14:34:03 +0000

    Setup new problem directory

---
+++ 015/code.cpp
@@ -0,0 +1,29 @@
+/*
+Daily Coding Challenge #015
+---------------------------
+
+
+Run Using
+---------
+g++ 015/code.cpp -o bin/out && ./bin/out < 015/in.txt > 015/out.txt
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mk make_pair
+#define pb push_back
+#define Pi pair<int, int>
+#define Pl pair<long long, long long>
+#define vi vector<int>
+#define vl vector<long long>
+#define vb vector<bool>
+#define vp vector<pair<int, int>>
+#define vpl vector<pair<long long, long long>>
+#define F first
+#define S second
+#define min(a, b) ((a < b) ? a : b)
+const ll mod = 1000000007;
+
+int main() {
+
+}
