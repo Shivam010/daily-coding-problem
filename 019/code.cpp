a4c59ba6969eb0f99062b539a12feda7feee933e
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-22 00:44:51 +0530

    Solve 019

--- 019/code.cpp
+++ 019/code.cpp
@@ -1,7 +1,15 @@
 /*
 Daily Coding Challenge #019
 ---------------------------
+This problem was asked by Facebook.
 
+A builder is looking to build a row of `N` houses that can be of `K` different
+colors. He has a goal of minimizing cost while ensuring that no two neighboring
+houses are of the same color.
+
+Given an N by K matrix where the nth row and kth column represents the cost to
+build the nth house with kth color, return the minimum cost which achieves this
+goal.
 
 Run Using
 ---------
@@ -9,21 +17,120 @@ g++ 019/code.cpp -o bin/out && ./bin/out < 019/in.txt > 019/out.txt
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define mk make_pair
-#define pb push_back
-#define Pi pair<int, int>
-#define Pl pair<long long, long long>
 #define vi vector<int>
-#define vl vector<long long>
-#define vb vector<bool>
-#define vp vector<pair<int, int>>
-#define vpl vector<pair<long long, long long>>
-#define F first
-#define S second
-#define min(a, b) ((a < b) ? a : b)
-const ll mod = 1000000007;
+#define v2 vector<vi>
+#define pi pair<int, int>
+#define vp vector<pi>
+
+int rec(v2 &a, int n, int k, int in, vi &path) {
+    int ai = in / k, aj = in % k;
+    if (in == -1) {
+        ai = -1;
+        aj = -1;
+    }
+    if (ai > n) {
+        return 0;
+    }
+    int ans = INT_MAX;
+    int cur = ai + 1;
+    if (cur == n - 1) {
+        for (int i = 0; i < k; i++) {
+            if (i != aj) {
+                if (ans > a[cur][i]) {
+                    path[cur] = i;
+                    ans = a[cur][i];
+                }
+            }
+        }
+        return ans;
+    }
+    for (int i = 0; i < k; i++) {
+        if (i != aj) {
+            int x = path[cur];
+            int preAns = a[cur][i] + rec(a, n, k, cur * k + i, path);
+            if (ans > preAns) {
+                ans = preAns;
+                path[cur] = i;
+            } else {
+                path[cur] = x;
+            }
+        }
+    }
+    return ans;
+}
+
+void solve_one(v2 &a, int n, int k) {
+    vi path(n + 1, 0);
+    cout << rec(a, n, k, -1, path) << endl;
+    for (int i = 0; i < n; i++) {
+        cout << a[i][path[i]] << "(" << path[i] << ") ";
+    }
+    cout << endl;
+}
+
+void modified(v2 &a, int n, int k) {
+    vp _path(n + 1);
+
+    pi preMin(0, -1), preSec(0, -1);
+
+    for (int i = 0; i < n; i++) {
+        pi curMin(INT_MAX, -1), curSec(INT_MAX, -1);
+        // cout << preMin.first << " " << preMin.second << endl;
+
+        for (int j = 0; j < k; j++) {
+            if (preMin.second != j)  // different index -> can have same color
+                a[i][j] += preMin.first;
+            else  // same index -> must have different color
+                a[i][j] += preSec.first;
+
+            if (curMin.first > a[i][j]) {
+                swap(curMin, curSec);
+                curMin.first = a[i][j];
+                curMin.second = j;
+            } else if (curSec.first > a[i][j]) {
+                curSec.first = a[i][j];
+                curSec.second = j;
+            }
+        }
+
+        _path[i].first = curMin.second;
+        _path[i].second = curSec.second;
+        if (preMin.second == _path[i].first)
+            swap(_path[i].first, _path[i].second);
+
+        swap(preMin, curMin);
+        swap(preSec, curSec);
+    }
+
+    cout << preMin.first << endl;
+    vi path(n, 0);
+    path[n - 1] = _path[n - 1].first;
+    for (int i = n - 2; i >= 0; i--) {
+        path[i] = _path[i].first;
+        if (path[i + 1] == path[i]) path[i] = _path[i].second;
+    }
+
+    int pre = 0;
+    for (int i = 0; i < n; i++) {
+        a[i][path[i]]-=pre;
+        pre += a[i][path[i]];
+        cout << a[i][path[i]] << "(" << path[i] << ") ";
+    }
+
+    cout << endl;
+}
 
 int main() {
-
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        v2 a(n, vi(k, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) cin >> a[i][j];
+        }
+        // solve_one(a, n, k);
+        modified(a, n, k);
+    }
 }
