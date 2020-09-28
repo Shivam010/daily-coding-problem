151fae1811d9e8de7a9ddca41af1e61bba0efcbc
Shivam Rathore <shivam.rathore010@gmail.com>
2020-09-28 14:34:22 +0000

    Setup new problem directory

---
+++ 026/code.cpp
@@ -0,0 +1,25 @@
+/*
+Daily Coding Challenge #026
+---------------------------
+
+
+Run Using
+---------
+g++ 026/code.cpp -o bin/out && ./bin/out < 026/in.txt > 026/out.txt
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    int n;
+    return;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ 026/code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.7
+
+"""
+Daily Coding Challenge #026
+---------------------------
+
+
+Run Using
+---------
+./026/code.py
+"""
+
+
