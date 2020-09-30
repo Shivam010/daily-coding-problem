dced806b545e6b8a05203903d392055b2733044d
Shivam Rathore <shivam.rathore010@gmail.com>
2020-10-01 00:37:21 +0530

    Solve 027

--- 027/code.cpp
+++ 027/code.cpp
@@ -1,7 +1,14 @@
 /*
 Daily Coding Challenge #027
 ---------------------------
+This problem was asked by Facebook.
 
+Given a string of round, curly, and square open and closing brackets, return
+whether the brackets are balanced (well-formed).
+
+For example, given the string `"([])[]({})"`, you should return true.
+
+Given the string `"([)]"` or `"((()"`, you should return false.
 
 Run Using
 ---------
@@ -12,7 +19,28 @@ using namespace std;
 #define ll long long
 
 void solve() {
-    int n;
+    string brk;
+    cin >> brk;
+    int n = brk.size(), top = 0;
+    string stk(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        if (stk[top] == brk[i])
+            top--;
+        else if (brk[i] == '(')
+            stk[++top] = ')';
+        else if (brk[i] == '[')
+            stk[++top] = ']';
+        else if (brk[i] == '{')
+            stk[++top] = '}';
+        else {
+            top = -1;
+            break;
+        }
+    }
+    if (top == 0)
+        cout << "true\n";
+    else
+        cout << "false\n";
     return;
 }
 
