0387b710143cf13c2ef5c993330efbc948e2f17e
Shivam Rathore <shivam.rathore010@gmail.com>
2020-10-04 03:37:31 +0530

    Solve 030 & 031

--- 031/code.cpp
+++ 031/code.cpp
@@ -1,7 +1,15 @@
 /*
 Daily Coding Challenge #031
 ---------------------------
+This problem was asked by Google.
 
+The edit distance between two strings refers to the minimum number of character
+insertions, deletions, and substitutions required to change one string to the
+other. For example, the edit distance between “kitten” and “sitting” is three:
+substitute the `“k”` for `“s”`, substitute the `“e”` for `“i”`, and append a
+`“g”`.
+
+Given two strings, compute the edit distance between them.
 
 Run Using
 ---------
@@ -10,9 +18,43 @@ g++ 031/code.cpp -o bin/out && ./bin/out < 031/in.txt > 031/out.txt
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define vi vector<int>
 
 void solve() {
-    int n;
+    string a, b;
+    cin >> a >> b;
+    int n = a.size(), m = b.size();
+    vi mat[n + 1];
+    for (int i = 0; i <= n; i++) {
+        if (i == 0) {
+            for (int j = 0; j <= m; j++) mat[i].push_back(j);
+            continue;
+        }
+        for (int j = 0; j <= m; j++) mat[i].push_back(0);
+        mat[i][0] = i;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            int val = mat[i][j - 1] + 1;
+            if (val > mat[i - 1][j] + 1) val = mat[i - 1][j] + 1;
+            int sub = (a[i - 1] != b[j - 1]);
+            if (val > mat[i - 1][j - 1] + sub) val = mat[i - 1][j - 1] + sub;
+            mat[i][j] = val;
+        }
+    }
+    cout << endl;
+    cout << "==> " << mat[n][m] << endl;
+    cout << "    ";
+    for (int j = 0; j < m; j++) cout << b[j] << " ";
+    cout << endl;
+    for (int i = 0; i <= n; i++) {
+        if (i != 0)
+            cout << a[i - 1] << " ";
+        else
+            cout << "  ";
+        for (int j = 0; j <= m; j++) cout << mat[i][j] << " ";
+        cout << endl;
+    }
     return;
 }
 
