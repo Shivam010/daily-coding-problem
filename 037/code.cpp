84fa9b918d6e099b9232e47c265cc3ceee3d17ed
Shivam Rathore <shivam.rathore010@gmail.com>
2020-10-11 22:13:51 +0530

    Solve 037

--- 037/code.cpp
+++ 037/code.cpp
@@ -1,7 +1,15 @@
 /*
 Daily Coding Challenge #037
 ---------------------------
+This problem was asked by Google.
 
+The power set of a set is the set of all its subsets. Write a function that,
+given a set, generates its power set.
+
+For example, given the set `{1, 2, 3}`, it should return `{{}, {1}, {2}, {3},
+{1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}`
+
+You may also use a list or array to represent a set.
 
 Run Using
 ---------
@@ -11,8 +19,26 @@ g++ 037/code.cpp -o bin/out && ./bin/out < 037/in.txt > 037/out.txt
 using namespace std;
 #define ll long long
 
-void solve() {
+void solution() {
     int n;
+    cin >> n;
+    ll a[n], mask[n];
+    mask[n] = 1 << n;
+    for (int i = 0; i < n; i++) cin >> a[i], mask[i] = 1 << i;
+    ll sz = 1 << n;
+    cout << n << endl;
+    while (sz--) {
+        cout << "{";
+        bool comma = false;
+        for (int i = 0; i < n; i++) {
+            if (sz & mask[i]) {
+                if (comma) cout << ",";
+                cout << a[i];
+                comma = true;
+            }
+        }
+        cout << "}\n";
+    }
     return;
 }
 
@@ -20,6 +46,6 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        solve();
+        solution();
     }
 }
