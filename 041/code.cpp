1139b9aa94f7d82cb1c119d0f366c1cffe6baa91
Shivam Rathore <shivam.rathore010@gmail.com>
2020-10-14 02:12:23 +0530

    Solve 041

--- 041/code.cpp
+++ 041/code.cpp
@@ -1,7 +1,25 @@
 /*
 Daily Coding Challenge #041
 ---------------------------
+This problem was asked by Facebook.
 
+Given an unordered list of flights taken by someone, each represented as
+(origin, destination) pairs, and a starting airport, compute the person's
+itinerary. If no such itinerary exists, return null. If there are multiple
+possible itineraries, return the lexicographically smallest one. All flights
+must be used in the itinerary.
+
+For example, given the list of flights [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL',
+'YYZ'), ('HKO', 'ORD')] and starting airport 'YUL', you should return the list
+['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
+
+Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport
+'COM', you should return null.
+
+Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and
+starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even
+though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first
+one is lexicographically smaller.
 
 Run Using
 ---------
@@ -9,16 +27,49 @@ g++ 041/code.cpp -o bin/out && ./bin/out < 041/in.txt > 041/out.txt
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+
+bool backtrack(int n, int noUsed, pair<string, string> *list, bool *used,
+               string *it, string last) {
+    if (noUsed == n) return true;
+    for (int i = 0; i < n; i++) {
+        if (used[i]) continue;
+        if (last == list[i].first) {
+            used[i] = true;
+            it[++noUsed] = list[i].second;
+            if (backtrack(n, noUsed, list, used, it, list[i].second))
+                return true;
+        }
+    }
+
+    return false;
+}
 
 void solve() {
-    int n;
-    return;
+    int n, sz = 0;
+    cin >> n;
+    pair<string, string> list[n];
+    bool used[n];
+    for (int i = 0; i < n; i++) {
+        cin >> list[i].first >> list[i].second;
+        used[i] = false;
+    }
+    sort(list, list + n);
+    string start;
+    cin >> start;
+    string it[n+1];
+    it[0] = start;
+    if (backtrack(n, 0, list, used, it, start))
+        for (int i = 0; i <=n; i++) cout << it[i] << " ";
+    else
+        cout << -1;
+    cout << endl;
 }
 
 int main() {
     int t;
     cin >> t;
+    fflush(stdin);
+    cin.ignore();
     while (t--) {
         solve();
     }
