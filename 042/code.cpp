b719027944f7b1c3a2fc928bc17bb969ae57a05d
Shivam Rathore <shivam.rathore010@gmail.com>
2020-10-14 14:37:21 +0000

    Setup new problem directory

---
+++ 042/code.cpp
@@ -0,0 +1,25 @@
+/*
+Daily Coding Challenge #042
+---------------------------
+
+
+Run Using
+---------
+g++ 042/code.cpp -o bin/out && ./bin/out < 042/in.txt > 042/out.txt
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve() {
+    int n;
+    return;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ 042/code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.7
+
+"""
+Daily Coding Challenge #042
+---------------------------
+
+
+Run Using
+---------
+./042/code.py
+"""
+
+
